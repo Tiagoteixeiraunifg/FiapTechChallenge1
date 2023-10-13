f74ac2579cdcea12d85e3dc80a70d3d50c796f21
PedroLucasCruz <pedrolucas11@live.com>
2023-10-12 21:30:10 -0300

    Configurações do DataService para que quando a aplicação for inicializada se não existe banco de dados criado seja criado sem a necessidade de rodar a migração.

---
+++ Biblioteca.Infraestrutura/Dados/ServicoDados/Implementacoes/DataService.cs
@@ -0,0 +1,25 @@
+﻿using Biblioteca.Infraestrutura.Dados.Contextos;
+using Biblioteca.Infraestrutura.Dados.ServicoDados.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Infraestrutura.Dados.ServicoDados.Implementacoes
+{
+    public class DataService : IDataService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public DataService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public void InicializarBd()
+        {
+            _applicationDbContext.Database.EnsureCreated();
+        }
+    }
+}
